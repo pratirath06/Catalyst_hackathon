bc4f4e53ea39e96e2c293be35d180ab4a53636f9
pratirath06 <pratirathgupta51@gmail.com>
2025-03-27 12:13:51 +0530

    feat: interactive

--- src/components/voice_input.py
+++ src/components/voice_input.py
@@ -1,20 +1,6 @@
 import streamlit as st
-from streamlit_mic_recorder import speech_to_text
 
 def get_voice_input():
     st.subheader("Input Options")
-    col1, col2 = st.columns([3, 1])
-
-    with col1:
-        text_input = st.chat_input("Type your question here...")
-
-    with col2:
-        voice_text = speech_to_text(
-            language='en',
-            start_prompt="🎤 Speak",
-            stop_prompt="Stop",
-            just_once=True,
-            key="voice_input"  # Fixed key since we only have one chat
-        )
-
-    return voice_text if voice_text else text_input
+    text_input = st.chat_input("Type your question here...")
+    return text_input
